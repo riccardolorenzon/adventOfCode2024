5f8af2bfa74c4ccaf092dcff5e5befd0899681ec
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2024-12-14 00:33:03 +0100

    05 part 2

--- 05/main.py
+++ 05/main.py
@@ -1,41 +1,69 @@
 from collections import defaultdict
 from io import TextIOWrapper
 import sys
+from typing import List
 
 
-def check_update_order(ordering, update):
+def check_update_order(cannot_be_before, update):
     for i, element in enumerate(update):
-        if len(set(update[:i]).intersection(ordering[element])) != 0:
+        wrong_position_elements = set(update[:i]).intersection(
+            cannot_be_before[element]
+        )
+        if len(wrong_position_elements) != 0:
             return False
     return True
 
 
-def part_1(ordering, updates):
+def part_1(cannot_be_before, updates):
     total_middle_elements = 0
     for update in updates:
-        if check_update_order(ordering, update):
+        check = check_update_order(cannot_be_before, update)
+        if check:
             total_middle_elements += int(update[len(update) // 2])
     print(total_middle_elements)
 
 
-def part_2(): ...
+def part_2(cannot_be_before, updates: List[List[str]]):
+    total_middle_elements = 0
+    for update in updates:
+        is_fixed = False
+        i = 0
+        while i < len(update):
+            page = update[i]
+            j = 0
+            sh_el = []
+            while j < i:
+                if update[j] in cannot_be_before[page]:
+                    is_fixed = True
+                    sh_el.append(update.pop(j))
+                    i -= 1
+                else:
+                    j += 1
+            update[i + 1 : i + 1] = sh_el
+            i += 1
+
+        if is_fixed:
+            total_middle_elements += int(update[len(update) // 2])
+            is_fixed = False
+
+    print(total_middle_elements)
 
 
 def process_input(f: TextIOWrapper):
-    ordering = defaultdict(set)
+    cannot_be_before = defaultdict(set)
     while line := f.readline():
         line = line.rstrip()
         if line == "":
             break
         val1, val2 = line.split("|")
-        ordering[val1].add(val2)
+        cannot_be_before[val1].add(val2)
     updates = [line.split(",") for line in f.read().splitlines()]
-    return ordering, updates
+    return cannot_be_before, updates
 
 
 if __name__ == "__main__":
     input_file = sys.argv[1]
     with open(input_file) as f:
-        ordering, updates = process_input(f)
-
-    part_1(ordering, updates)
+        cannot_be_before, updates = process_input(f)
+    part_1(cannot_be_before, updates)
+    part_2(cannot_be_before, updates)
