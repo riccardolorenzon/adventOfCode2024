697212dc306d56808888c3d94a1c72b07af7f69c
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2025-02-04 15:25:34 +0100

    23, part 1

---
+++ 23/main.py
@@ -0,0 +1,59 @@
+# the idea is to introduce a new class
+#   LanNode, containing:
+# - list of other nodes it's connected to.
+# for each new connection x - y
+# get node x
+# get node y
+# check if x and y have any node they are both connected to
+# if so -> add to ternaries set
+import sys
+from collections import defaultdict
+
+
+def common_connected_nodes(
+    connected_nodes: list[str], other_connected_nodes: list[str]
+):
+    for node in connected_nodes:
+        if node in other_connected_nodes:
+            yield node
+
+
+def read_input() -> list[tuple[str, str]]:
+    res = []
+    with open(sys.argv[1]) as f:
+        while line := f.readline().strip():
+            n1, n2 = line.split("-")
+            res.append((n1, n2))
+    return res
+
+
+def map_nodes() -> dict:
+    lan_nodes = defaultdict(list)
+    for node_1, node_2 in read_input():
+        lan_nodes[node_1].append(node_2)
+        lan_nodes[node_2].append(node_1)
+    return lan_nodes
+
+
+def get_ternary_sets(lan_nodes: dict[str, list]):
+    ternary_sets = set()
+    for node, connected_nodes in lan_nodes.items():
+        for connected_node in connected_nodes:
+            for common_node in common_connected_nodes(
+                connected_nodes, lan_nodes[connected_node]
+            ):
+                ternary_sets.add(tuple(sorted([node, connected_node, common_node])))
+    return ternary_sets
+
+
+if __name__ == "__main__":
+    lan_nodes = map_nodes()
+    print(
+        len(
+            [
+                t
+                for t in get_ternary_sets(lan_nodes)
+                if "t" in [t[0][0], t[1][0], t[2][0]]
+            ]
+        )
+    )
